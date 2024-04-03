44a3fbe68c950f19c4a29bc07441819c6991e80d
VladZimin <vlad.zimin1@gmail.com>
2024-04-03 18:23:38 +0600

    corrected warnings

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,6 +1,5 @@
 import {BuildOptions} from './types/config'
 import {Configuration as DevServerConfiguration} from 'webpack-dev-server'
-import path from 'path'
 
 export const buildDevServer = ({port}: BuildOptions): DevServerConfiguration => {
     return {
