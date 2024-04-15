b5c7d591d45ae94da0660dad156248017ba21b47
VladZimin <vlad.zimin1@gmail.com>
2024-04-15 22:29:48 +0600

    added jest, first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,23 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('with additional class', () => {
+        const expected = 'someClass class1 class2';
+        expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with mods', () => {
+        const expected = 'someClass class1 class2 collapsed hovered';
+        expect(classNames('someClass', { collapsed: true, hovered: true }, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with mods false', () => {
+        const expected = 'someClass class1 class2 collapsed';
+        expect(classNames('someClass', { collapsed: true, hovered: false }, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with mods undefined', () => {
+        const expected = 'someClass class1 class2 collapsed';
+        expect(classNames('someClass', { collapsed: true, hovered: undefined }, ['class1', 'class2'])).toBe(expected);
+    });
+});
